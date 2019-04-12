42c5667cfd82db5ac85d1b4c589d35be949f1448
Khue Doan <khuedoan98@gmail.com>
2019-04-12 13:07:12 +0700

    Change font

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-static int borderpx = 2;
+static char *font = "SauceCodePro Nerd Font Mono:pixelsize=13:style=Semibold:antialias=true:autohint=true";
+static int borderpx = 0;
 
 /*
  * What program is execed by st depends of these precedence rules:
