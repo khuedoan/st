aa1111308d23062ed9178c9fe15a8e7575938015
Khue Doan <khuedoan98@gmail.com>
2019-05-15 23:50:44 +0700

    Use semibold Sauce Code Pro font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "SauceCodePro Nerd Font:style=Semibold:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
