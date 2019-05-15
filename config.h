3dff44e3989d4d550ef9e66cb928dbfcc0422f1e
Khue Doan <khuedoan98@gmail.com>
2019-05-16 01:02:37 +0700

    Change font style to Medium
    Semibold cause some problems

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "SauceCodePro Nerd Font:style=Semibold:pixelsize=14:antialias=true:autohint=true";
+static char *font = "SauceCodePro Nerd Font:style=Medium:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
