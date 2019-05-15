6dd8e17d66a0a14c564b112f9d38bca0c4dc9ced
Khue Doan <khuedoan98@gmail.com>
2019-05-15 23:25:27 +0700

    Add Dracula foreground and background colors

--- config.h
+++ config.h
@@ -107,8 +107,8 @@ static const char *colorname[] = {
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+	"#F8F8F2",
+	"#282A36",
 };
 
 
@@ -116,8 +116,8 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
 static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
