d17397525c656c522ba7d11f671bae26a6f273e1
Khue Doan <khuedoan98@gmail.com>
2020-04-29 08:34:18 +0700

    Increase border to 10px

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "SauceCodePro Nerd Font:style=Medium:pixelsize=14:antialias=true:autohint=true";
-static int borderpx = 2;
+static int borderpx = 10;
 
 /*
  * What program is execed by st depends of these precedence rules:
