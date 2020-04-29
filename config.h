d2188026e786214fbdfcb3436fea9aa253634b79
Khue Doan <khuedoan98@gmail.com>
2020-04-29 11:08:01 +0700

    Add newterm patch

--- config.h
+++ config.h
@@ -184,6 +184,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ (Mod4Mask|ShiftMask), XK_Return,      newterm,        {.i =  0} },
 };
 
 /*
