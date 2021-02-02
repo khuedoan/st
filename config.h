3790eac72cbdac5c35e3cf4c73ff4146cfc5355f
Khue Doan <khuedoan98@gmail.com>
2021-02-03 01:50:52 +0700

    Replace Source Code Pro with Fira Code font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "SauceCodePro Nerd Font Mono:style=Medium:pixelsize=14:antialias=true:autohint=true";
+static char *font = "FiraCode Nerd Font Mono:style=Medium:pixelsize=13:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
